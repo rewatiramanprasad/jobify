0a253a047cd84a75c2d8ad52f51d0d726e7e3524
Raman Prasad <ramanprasad.0203@gmail.com>
2025-03-12 13:07:51 +0530

    refactor: remove log

--- utils/actions.tsx
+++ utils/actions.tsx
@@ -3,10 +3,9 @@ import { redirect } from 'next/navigation'
 import { createAndEditJobSchema, createAndEditJobType, JobType } from './types'
 import db from '@/utils/db'
 import { auth } from '@clerk/nextjs/server'
+import { Prisma } from '@prisma/client'
 export async function authenticateAndRedirect(): Promise<string> {
-  console.log('hello....')
   const { userId } = await auth()
-  console.log(userId)
   if (!userId) {
     redirect('/')
   }
@@ -17,12 +16,9 @@ export async function createJobAction(
   values: createAndEditJobType
 ): Promise<JobType | null> {
   //   await new Promise((resolve) => setTimeout(resolve, 3000))
-  console.log('working 3')
   const userId = await authenticateAndRedirect()
-  console.log(userId)
   try {
     createAndEditJobSchema.parse(values)
-    console.log('working 4')
     const job: JobType = await db.job.create({
       data: {
         ...values,
@@ -30,10 +26,73 @@ export async function createJobAction(
         clerkId: userId,
       },
     })
-    console.log('working 5')
     return job
   } catch (error) {
     console.log(error)
     return null
+  }
+}
+
+type getAllJobActionTypes = {
+  search?: string
+  jobStatus?: string
+  page?: number
+  limit?: number
+}
+
+export async function getAllJobAction({
+  search,
+  jobStatus,
+  page = 1,
+  limit = 10,
+}: getAllJobActionTypes): Promise<{
+  jobs: JobType[]
+  count: number
+  page: number
+  totalPages: number
+}> {
+  const userId = await authenticateAndRedirect()
+  try {
+    let whereClause: Prisma.JobWhereInput = {
+      clerkId: userId,
+    }
+    if (search) {
+      whereClause = {
+        ...whereClause,
+        OR: [
+          {
+            position: {
+              contains: search,
+            },
+          },
+          {
+            company: {
+              contains: search,
+            },
+          },
+        ],
+      }
+    }
+
+    if (jobStatus && jobStatus !== 'all') {
+      whereClause = {
+        ...whereClause,
+        status: jobStatus,
+      }
+    }
+    const skip = (page - 1) * limit
+
+    const jobs = await db.job.findMany({
+      skip: skip,
+      take: limit,
+      where: whereClause,
+      orderBy: { createdAt: 'desc' },
+    })
+    const count: number = await db.job.count({ where: whereClause })
+    const totalPages = Math.ceil(count / limit)
+    return { jobs: jobs, count: count, page, totalPages: totalPages }
+  } catch (error) {
+    console.error(error)
+    return { jobs: [], count: 0, page: 1, totalPages: 0 }
   }
 }
