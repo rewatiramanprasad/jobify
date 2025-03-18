0ff7473564335ccec2f51bdb949e04f854499a85
Raman Prasad <ramanprasad.0203@gmail.com>
2025-03-18 21:35:57 +0530

    feat: added pagination

--- components/JobList.tsx
+++ components/JobList.tsx
@@ -4,6 +4,8 @@ import { useQuery } from '@tanstack/react-query'
 import { useSearchParams } from 'next/navigation'
 import React from 'react'
 import JobCard from './JobCard'
+import ButtonContainer from './ButtonContainer'
+import ComplexButtonContainer from './ComplexButtonContainer'
 
 function JobList() {
   const searchParams = useSearchParams()
@@ -14,11 +16,21 @@ function JobList() {
     queryKey: ['jobs'],
     queryFn: () => getAllJobAction({}),
   })
+  const count = data?.count || 0
+  const totalPages = data?.totalPages || 0
   const jobs = data?.jobs || []
   if (isPending) return <h2 className="text-xl">Please Wait...</h2>
   if (jobs.length < 1) return <h2 className="text-xl"> No Jobs found...</h2>
   return (
     <>
+      <div className="flex items-center justify-between  mt-8 mb-8">
+        <h2 className="text-xl font-semibold capitalize ">
+          {count} jobs found
+        </h2>
+        {totalPages < 2 ? null : (
+          <ComplexButtonContainer currentPage={page} totalPages={totalPages} />
+        )}
+      </div>
       <div className="grid md:grid-cols-2  gap-8">
         {jobs.map((job) => (
           <JobCard key={job.id} job={job} />
